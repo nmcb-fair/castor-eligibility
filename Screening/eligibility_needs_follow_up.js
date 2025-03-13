84073dd13d8cc35ec1faee53f493a9c2e7bf3954
Sjoerd Manger <sjoerdmanger@gmail.com>
2025-03-13 10:39:55 +0100

    Update eligibility_needs_follow_up.js

--- Screening/eligibility_needs_follow_up.js
+++ Screening/eligibility_needs_follow_up.js
@@ -4,7 +4,14 @@
 var eligibility_fu = 0;
 var splitted = "{meds01}".split(';');
 
-if (splitted.indexOf("9") == 1) { // other medication
+if (splitted.indexOf("2") > -1) { // antibiotics
+	// medication (to) stop(ped)?
+	if ({Antibiotics_stop} == 0) {		
+		eligibility_fu = 1;
+	}
+}
+
+if (splitted.indexOf("9") > -1) { // other medication
 	// medication (to) stop(ped)?
 	if ({Other_stop} == 0) {		
 		eligibility_fu = 1;
