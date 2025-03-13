4fd8eaaf524b090fba72ec48af9f05a6f6c55289
Sjoerd Manger <sjoerdmanger@gmail.com>
2025-03-13 10:57:46 +0100

    Update eligible_later.js

--- Screening/eligible_later.js
+++ Screening/eligible_later.js
@@ -4,31 +4,40 @@
 var eligible_later = 0;
 var splitted = "{meds01}".split(';');
 
-if (splitted.indexOf("2") == 1) { // antibiotics	
+if (splitted.indexOf("2") > -1 ) { // antibiotics	
+       // medication (to) stop(ped)?
+	if ({Antibiotics_stop} == 1) {		
+		eligible_later  = 1;
+	}
+}
+else if (splitted.indexOf("3") > -1 ) { // antiviral
+       // medication (to) stop(ped)?
+	if ({Antiviral_stop} == 1) {		
+		eligible_later  = 1;
+	}
+}
+else if (splitted.indexOf("4") > -1 ) { // vaccines
 	eligible_later = 1;
 }
-else if (splitted.indexOf("3") == 1) { // antiviral
-	eligible_later = 1;
-}
-else if (splitted.indexOf("4") == 1) { // vaccines
-	eligible_later = 1;
-}
-else if (splitted.indexOf("5") == 1) { // opioid
+else if (splitted.indexOf("5") > -1 ) { // opioid
 	// medication (to) stop(ped)?
 	if ({Opioids_stop} == 1) {		
 		eligible_later = 1;
 	}
 }
-else if (splitted.indexOf("8") == 1) { // therapeutic steroids
-	eligible_later = 1;
+else if (splitted.indexOf("8") > -1 ) { // therapeutic steroids
+	// medication (to) stop(ped)?
+	if ({Therapeuticsteriods_stop} == 1) {		
+		eligible_later = 1;
+	}
 }
-else if (splitted.indexOf("9") == 1) { // other medication
+else if (splitted.indexOf("9") > -1 ) { // other medication
 	// medication (to) stop(ped)?
 	if ({Other_stop} == 1) {		
 		eligible_later = 1;
 	}
 }
-else if (splitted.indexOf("13") == 1) { // antipsychotics
+else if (splitted.indexOf("13") > -1 ) { // antipsychotics
 	// medication (to) stop(ped)?
 	if ({Antipsychotics_stop} == 1) {		
 		eligible_later = 1;
@@ -40,9 +49,8 @@ if ({pregbrstfeed} == 1) {
 	eligible_later = 1;
 }
 
-// Post/Long COVID
-var splitted = "{gmh_diagnosis01}".split(';');
-if (splitted.indexOf("5") == 1) {
+// Current COVID
+if ({gmh_currcovid} == 1 ) {
 	eligible_later = 1;
 }
 
